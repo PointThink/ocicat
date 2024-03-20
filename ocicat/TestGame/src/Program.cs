5a6d6c2cb46930a6ad9e9ce12e5bc9f93bae01b0
PointThink <69512713+pointThink@users.noreply.github.com>
2024-03-20 21:20:16 +0100

    Indices

--- ocicat/TestGame/src/Program.cs
+++ ocicat/TestGame/src/Program.cs
@@ -81,6 +81,16 @@ class Program
 			0.5f, -0.5f, 0.0f, //Bottom-right vertex
 			0.0f,  0.5f, 0.0f  //Top vertex
 		};
+
+		uint[] indices =
+		{
+			0, 1, 2, 3
+		};
+
+		int ElementBufferObject;
+		ElementBufferObject = GL.GenBuffer();
+		GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+		GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
 		
 		int VertexBufferObject;
 		VertexBufferObject = GL.GenBuffer();
@@ -118,6 +128,9 @@ void main()
 		GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
 		GL.EnableVertexAttribArray(0);
 
+		GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+		GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+		
 		shader.Use();
 		
 		while (!window.ShouldClose())
@@ -125,7 +138,7 @@ void main()
 			window.HandleEvents();
 			
 			GL.Clear(ClearBufferMask.ColorBufferBit);
-			GL.DrawArrays(PrimitiveType.Triangles, 0, 3);			
+			GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);			
 			
 			window.SwapBuffers();
 		}
