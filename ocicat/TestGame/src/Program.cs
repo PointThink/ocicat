6bf210c2de0d934b95fef7f92f41bc07cbd020d0
PointThink <69512713+pointThink@users.noreply.github.com>
2024-03-20 21:03:14 +0100

    Logging

---
+++ ocicat/TestGame/src/Program.cs
@@ -0,0 +1,25 @@
+﻿using ocicat;
+using ocicat.Graphics;
+
+namespace TestGame;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Window window = new Window("Hello", 800, 600);
+
+		Logging.LogLevel = LogLevel.Developer;
+		
+		Logging.Log(LogLevel.Developer, "Hello");
+		Logging.Log(LogLevel.Info, "Hello");
+		Logging.Log(LogLevel.Warning, "Hello");
+		Logging.Log(LogLevel.Error, "Hello");
+		
+		while (!window.ShouldClose())
+		{
+			window.HandleEvents();
+			window.SwapBuffers();
+		}
+	}
+}
---
+++ ocicat/ocicat/src/Log.cs
@@ -0,0 +1,46 @@
+namespace ocicat;
+
+public enum LogLevel
+{
+	Developer = 0,
+	Info = 1,
+	Warning = 2,
+	Error = 3,
+}
+
+class LogLevelInfo
+{
+	public LogLevelInfo(string label, ConsoleColor color)
+	{
+		Label = label;
+		Color = color;
+	}
+		
+	public string Label;
+	public ConsoleColor Color;
+}
+
+public class Logging
+{
+	public static LogLevel LogLevel = LogLevel.Info;
+
+	private static Dictionary<LogLevel, LogLevelInfo> _logLevelInfo = new Dictionary<LogLevel, LogLevelInfo>()
+	{
+		{ LogLevel.Developer, new LogLevelInfo("Developer", ConsoleColor.Cyan) },
+		{ LogLevel.Info, new LogLevelInfo("Info", ConsoleColor.White) },
+		{ LogLevel.Warning, new LogLevelInfo("Warning", ConsoleColor.Yellow) },
+		{ LogLevel.Error, new LogLevelInfo("Error", ConsoleColor.Red) }
+	};
+	
+	public static void Log(LogLevel level, string text)
+	{
+		if (level >= LogLevel)
+		{
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.BackgroundColor = _logLevelInfo[level].Color;
+			Console.Write($" {_logLevelInfo[level].Label} ");
+			Console.ResetColor();
+			Console.WriteLine($" {text}");
+		}
+	}
+}
