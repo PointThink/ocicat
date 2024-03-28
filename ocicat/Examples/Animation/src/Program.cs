3c525a5bc64b35bf10a13f31a12d23eb0b524a78
PointThink <69512713+pointThink@users.noreply.github.com>
2024-03-28 18:48:54 +0100

    Added more examples

---
+++ ocicat/Examples/Animation/src/Program.cs
@@ -0,0 +1,35 @@
+﻿using ocicat;
+using ocicat.Graphics;
+using ocicat.Graphics.Rendering;
+
+namespace Animation;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Window window = Window.Create("Hello world", 800, 600);
+		Renderer renderer = new Renderer(window);
+
+		// created once per animation
+		AnimationTemplate template = new AnimationTemplate([
+			Texture.Create(renderer, "image.jpg"),
+			Texture.Create(renderer, "image2.png"),
+		], 1);
+
+		// create once per object that uses the animation
+		AnimationController animationController = new AnimationController(template);
+		
+		while (!window.ShouldClose())
+		{
+			window.HandleEvents();
+			renderer.BeginDrawing();
+			
+			renderer.ClearScreen(Color.CreateRGBA8(255, 255, 255, 255));
+			animationController.Draw(renderer, new Vector2(0, 0), new Vector2(256, 256));
+			
+			renderer.EndDrawing();
+			window.Present();
+		}
+	}
+}
---
+++ ocicat/Examples/Textures/src/Program.cs
@@ -0,0 +1,33 @@
+﻿using ocicat;
+using ocicat.Graphics.Rendering;
+using ocicat.Graphics;
+
+namespace Textures;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Window window = Window.Create("Hello world", 800, 600);
+		Renderer renderer = new Renderer(window);
+		
+		Texture texture = Texture.Create(renderer, "image.jpg");
+		
+		while (!window.ShouldClose())
+		{
+			window.HandleEvents();
+			renderer.BeginDrawing();
+			
+			renderer.ClearScreen(Color.CreateRGBA8(255, 255, 255, 255));
+			
+			renderer.DrawRectTextured(
+				new Vector2(0, 0), 
+				new Vector2(texture.GetWidth(), texture.GetWidth()),
+				texture
+			);
+			
+			renderer.EndDrawing();
+			window.Present();
+		}
+	}
+}
