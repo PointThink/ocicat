90d53b570f8614253a29b976b60b27415c897ee6
PointThink <69512713+pointThink@users.noreply.github.com>
2024-04-06 17:19:00 +0200

    Added audio example

---
+++ ocicat/Examples/Audio/src/Program.cs
@@ -0,0 +1,31 @@
+﻿using ocicat;
+using ocicat.Audio;
+using ocicat.Input;
+
+namespace Audio;
+
+class InGame : GameState
+{
+	public override void Update()
+	{
+		if (Game.Window.IsKeyPressed(Key.Space))
+		{
+			Game.AudioEngine.PlaySound(Program.Sound);
+		}
+	}
+}
+
+class Program
+{
+	public static Sound Sound;
+	
+	static void Main(string[] args)
+	{
+		Game.Create("Press space to play sound", 800, 600);
+
+		Sound = new Sound(Game.AudioEngine, "erro.ogg");
+		
+		Game.GameState = new InGame();
+		Game.Run();
+	}
+}
